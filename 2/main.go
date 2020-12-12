a998c2b1303e49e5618b9400914cbd33237d04b1
Daniele Brugnara <daniele@brugnara.me>
2020-12-12 08:33:31 +0100

    day 2

--- 2/main.go
+++ 2/main.go
@@ -9,13 +9,41 @@ import (
 )
 
 func main() {
-	pt1()
-}
-
-func pt1() {
 	raw, _ := ioutil.ReadFile("./input.txt")
 	lines := strings.Split(string(raw), "\n")
 
+	pt1(lines)
+	pt2(lines)
+}
+
+func pt2(lines []string) {
+	valids := 0
+
+	for _, line := range lines {
+		re := regexp.MustCompile(`(\d+)\-(\d+) (\w): (\w+)`)
+		match := re.FindStringSubmatch(line)
+		if len(match) == 0 {
+			continue
+		}
+
+		char := match[3][0]
+		low, _ := strconv.Atoi(match[1])
+		high, _ := strconv.Atoi(match[2])
+
+		if match[4][low-1] == char && match[4][high-1] != char {
+			valids++
+			continue
+		}
+
+		if match[4][low-1] != char && match[4][high-1] == char {
+			valids++
+		}
+	}
+
+	fmt.Println("Valids:", valids)
+}
+
+func pt1(lines []string) {
 	valids := 0
 
 	for _, line := range lines {
@@ -33,10 +61,7 @@ func pt1() {
 		low, _ := strconv.Atoi(match[1])
 		high, _ := strconv.Atoi(match[2])
 		if count >= low && count <= high {
-			fmt.Println("Valid:", line)
 			valids++
-		} else {
-			fmt.Println("invalid:", line)
 		}
 	}
 	fmt.Println("total valids are:", valids)
