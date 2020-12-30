3ff61cb292bf4fdab3d5c4500c43ff2750c7429f
Daniele Brugnara <daniele@brugnara.me>
2020-12-30 23:01:23 +0100

    20.2 unfinished

--- 2020/20/main.go
+++ 2020/20/main.go
@@ -23,11 +23,19 @@ func getInput() []string {
 }
 
 type tile struct {
-	lines   [][]bool
+	id    int
+	lines [][]bool
+	// borders converted from bin to int, in 2 ways (LtoR, RtoL)
 	borders map[int][]int
 }
 
-func pt1(lines []string) (ret int) {
+type myHugeMap map[int]map[int]tile
+
+func (t *tile) reduce(x, y int) {
+	t.lines = t.lines[0:1]
+}
+
+func genHash(lines []string) map[int]tile {
 	hash := map[int]tile{}
 	re := regexp.MustCompile(`Tile (\d+):`)
 	current := 0
@@ -36,7 +44,7 @@ func pt1(lines []string) (ret int) {
 		match := re.FindStringSubmatch(line)
 		if len(match) > 0 {
 			if current != 0 {
-				hash[current] = tile{tmpLines, map[int][]int{}}
+				hash[current] = tile{current, tmpLines, map[int][]int{}}
 			}
 			current, _ = strconv.Atoi(match[1])
 			tmpLines = [][]bool{}
@@ -47,9 +55,12 @@ func pt1(lines []string) (ret int) {
 		}
 		tmpLines = append(tmpLines, lineToBool(line))
 	}
-	hash[current] = tile{tmpLines, map[int][]int{}}
+	// add the last one
+	hash[current] = tile{current, tmpLines, map[int][]int{}}
+
+	// populate the "borders"
 	for k, v := range hash {
-		hash[k].borders[0] = []int{
+		hash[k].borders[1] = []int{
 			toInt(v.lines[0]),
 			toInt(reverse(v.lines[0])),
 		}
@@ -65,7 +76,7 @@ func pt1(lines []string) (ret int) {
 			v.lines[8][0],
 			v.lines[9][0],
 		}
-		hash[k].borders[1] = []int{
+		hash[k].borders[0] = []int{
 			toInt(tmp),
 			toInt(reverse(tmp)),
 		}
@@ -92,22 +103,13 @@ func pt1(lines []string) (ret int) {
 			toInt(reverse(v.lines[9])),
 		}
 	}
-	// find borders with two disjoints
-	joints := map[int]int{}
-	for k1, v1 := range hash {
-		for _, vBorder := range v1.borders {
-			for k2, v2 := range hash {
-				for _, vBorder2 := range v2.borders {
-					if vBorder2[0] == vBorder[0] || vBorder2[0] == vBorder[1] || vBorder2[1] == vBorder[0] || vBorder2[1] == vBorder[1] {
-						if k1 != k2 {
-							joints[k1]++
-						}
-					}
-				}
-			}
-		}
-	}
-	fmt.Println(joints)
+	return hash
+}
+
+func pt1(lines []string) (ret int) {
+	hash := genHash(lines)
+	// find borders with two matches
+	joints := getJoints(hash)
 	ret = 1
 	for k, v := range joints {
 		if v == 2 {
@@ -115,6 +117,49 @@ func pt1(lines []string) (ret int) {
 		}
 	}
 	return
+}
+
+func getJoints(hash map[int]tile) map[int]int {
+	joints := map[int]int{}
+	for k1, v1 := range hash {
+		for _, vBorder := range v1.borders {
+			for k2, v2 := range hash {
+				if k2 == k1 {
+					continue
+				}
+				for _, vBorder2 := range v2.borders {
+					if vBorder2[0] == vBorder[0] || vBorder2[0] == vBorder[1] ||
+						vBorder2[1] == vBorder[0] || vBorder2[1] == vBorder[1] {
+						joints[k1]++
+					}
+				}
+			}
+		}
+	}
+	return joints
+}
+
+func getJoints2(hash map[int]tile) map[int]map[int]bool {
+	joints := map[int]map[int]bool{}
+	for k1, v1 := range hash {
+		for _, vBorder := range v1.borders {
+			for k2, v2 := range hash {
+				if k2 == k1 {
+					continue
+				}
+				for _, vBorder2 := range v2.borders {
+					if vBorder2[0] == vBorder[0] || vBorder2[0] == vBorder[1] ||
+						vBorder2[1] == vBorder[0] || vBorder2[1] == vBorder[1] {
+						if _, ok := joints[k1]; !ok {
+							joints[k1] = map[int]bool{}
+						}
+						joints[k1][k2] = true
+					}
+				}
+			}
+		}
+	}
+	return joints
 }
 
 func toInt(bArr []bool) (ret int) {
@@ -147,7 +192,294 @@ func lineToBool(s string) (ret []bool) {
 	return
 }
 
-func pt2(lines []string) (ret int) {
+func rotate(t tile, count int) tile {
+	if count <= 0 {
+		return t
+	}
 
+	ln := len(t.lines)
+	if ln != len(t.lines[0]) {
+		fmt.Println(ln, "!=", len(t.lines[0]))
+		panic("Y u do diss")
+	}
+
+	// rotating in-place, exchanging vertex
+	for offset := 0; offset < ln/2; offset++ {
+		for i := offset; i < ln-1-offset; i++ {
+			t.lines[offset][i], t.lines[i][ln-1-offset] = t.lines[i][ln-1-offset], t.lines[offset][i]
+			t.lines[i][ln-1-offset], t.lines[ln-1-offset][ln-1-i] = t.lines[ln-1-offset][ln-1-i], t.lines[i][ln-1-offset]
+			t.lines[ln-1-offset][ln-1-i], t.lines[ln-1-i][offset] = t.lines[ln-1-i][offset], t.lines[ln-1-offset][ln-1-i]
+		}
+	}
+
+	// exchanging borders too
+	t.borders[0], t.borders[1], t.borders[2], t.borders[3] =
+		t.borders[1], t.borders[2], t.borders[3], t.borders[0]
+
+	// then flip
+	t.borders[0][0], t.borders[0][1] = t.borders[0][1], t.borders[0][0]
+	t.borders[2][0], t.borders[2][1] = t.borders[2][1], t.borders[2][0]
+	return rotate(t, count-1)
+}
+
+func flip(t tile, direction byte) tile {
+	ln1 := len(t.lines)
+	ln2 := len(t.lines[0])
+	//
+	if direction == 'H' {
+		for i := 0; i < ln1; i++ {
+			for j := 0; j < ln2/2; j++ {
+				t.lines[i][j], t.lines[i][ln2-1-j] = t.lines[i][ln2-1-j], t.lines[i][j]
+			}
+		}
+		// exchanging borders too
+		t.borders[1][0], t.borders[1][1] = t.borders[1][1], t.borders[1][0]
+		t.borders[3][0], t.borders[3][1] = t.borders[3][1], t.borders[3][0]
+		t.borders[0], t.borders[2] = t.borders[2], t.borders[0]
+	} else {
+		for i := 0; i < ln2; i++ {
+			for j := 0; j < ln1/2; j++ {
+				t.lines[j][i], t.lines[ln2-1-j][i] = t.lines[ln2-1-j][i], t.lines[j][i]
+			}
+		}
+		// exchanging borders too
+		t.borders[0][0], t.borders[0][1] = t.borders[0][1], t.borders[0][0]
+		t.borders[2][0], t.borders[2][1] = t.borders[2][1], t.borders[2][0]
+		t.borders[1], t.borders[3] = t.borders[3], t.borders[1]
+	}
+
+	return t
+}
+
+func pt2(lines []string) (ret int) {
+	hash := genHash(lines)
+	fmt.Println(len(hash))
+	hugeMap := myHugeMap{}
+	first := 0
+	joints := getJoints2(hash)
+	for k, v := range joints {
+		if len(v) == 2 {
+			first = k
+			break
+		}
+	}
+	fmt.Println(first, joints[first], hugeMap)
+	fmt.Println(hash[first].borders)
+	for v := range joints[first] {
+		fmt.Println(hash[v].borders)
+	}
+	// rebuild the big image, starting from the first vertex
+	// at first, we need to rotate the image until borders 0 and 1 are on
+	// Right and Top (we are looking but these are always 0 and 1..)
+	noMatches := []int{}
+	for j := 0; j < 4; j++ {
+		b := hash[first].borders[j][0]
+		found := false
+		for v := range joints[first] {
+			// fmt.Println(hash[v].borders)
+			for i := 0; i < 4; i++ {
+				if b == hash[v].borders[i][0] || b == hash[v].borders[i][1] {
+					found = true
+					break
+				}
+			}
+		}
+
+		if !found {
+			noMatches = append(noMatches, j)
+		}
+	}
+	fmt.Println(noMatches)
+	/*
+			1
+		0		2
+			3
+	*/
+	// 1697 is a great choice to start from!
+	// current = 1697
+	current := first
+	fmt.Println(current)
+	count := 0
+	y := 0
+	x := 1
+	hugeMap[y] = map[int]tile{
+		0: hash[current],
+	}
+	for {
+		// for security reasons, always keep a break somewhere..
+		if count > 250 {
+			fmt.Println("Breaking!")
+			break
+		}
+		count++
+		//
+		// fmt.Println(hash[current].borders[2])
+		tmpTile, id := getMatchingTile(hugeMap[y][x-1], 2, &hash)
+		if id == -1 {
+			tmpTile, id = getMatchingTile(hugeMap[y][0], 3, &hash)
+			if id == -1 {
+				break
+			}
+			x = 0
+			y++
+			fmt.Println("New line! Prev has a len of:", len(hugeMap[y-1]), "and has y:", y)
+			hugeMap[y] = map[int]tile{}
+		}
+		hugeMap[y][x] = tmpTile
+		x++
+	}
+	fmt.Println(len(hugeMap), len(hugeMap[0]), "count:", count)
+
+	// this will print the map before cleanup!
+	printHugeMap(&hugeMap)
+
+	// zip the map, clearing boundaries
+	zipped := zipMap(&hugeMap)
+	fmt.Println(zipped, len(zipped), len(strings.Split(zipped, "\n")))
+
+	// find re!
+	reLine1 := regexp.MustCompile(`..................#.`)
+	reLine2 := regexp.MustCompile(`#....##....##....###`)
+	reLine3 := regexp.MustCompile(`.#..#..#..#..#..#...`)
+
+	match1 := reLine1.FindAllStringIndex(zipped, -1)
+	match2 := reLine2.FindAllStringIndex(zipped, -1)
+	match3 := reLine3.FindAllStringIndex(zipped, -1)
+
+	fmt.Println(len(match1), len(match2), len(match3))
+
+	fmt.Println(match2[1:])
+	fmt.Println(match3[1:])
 	return
+}
+
+func zipMap(hugeMap *myHugeMap) string {
+	ret := ""
+	h := len(*hugeMap)
+	w := len((*hugeMap)[0])
+	hh := len((*hugeMap)[0][0].lines)
+	ww := len((*hugeMap)[0][0].lines[0])
+	//
+	for y := 0; y < h; y++ {
+		for j := 0; j < hh; j++ {
+			line := ""
+			for x := 0; x < w; x++ {
+				for i := 0; i < ww; i++ {
+					if x > 0 && i == 0 {
+						continue
+					}
+					if y > 0 && j == 0 {
+						continue
+					}
+					chr := "."
+					if (*hugeMap)[y][x].lines[j][i] {
+						chr = "#"
+					}
+					line += chr
+				}
+			}
+			if len(line) > 0 {
+				ret += line + "\n"
+			}
+		}
+	}
+	return ret
+}
+
+func printHugeMap(hugeMap *myHugeMap) {
+	h := len(*hugeMap)
+	w := len((*hugeMap)[0])
+	hh := len((*hugeMap)[0][0].lines)
+	ww := len((*hugeMap)[0][0].lines[0])
+
+	for y := 0; y < h; y++ {
+		for j := 0; j < hh; j++ {
+			for x := 0; x < w; x++ {
+				for i := 0; i < ww; i++ {
+					chr := "."
+					if (*hugeMap)[y][x].lines[j][i] {
+						chr = "#"
+					}
+					fmt.Print(chr)
+				}
+			}
+			fmt.Println()
+		}
+	}
+
+}
+
+// see main_test.go # TestOutput placeholder
+func printHugeMapIDs(hugeMap *myHugeMap) {
+	h := len(*hugeMap)
+	w := len((*hugeMap)[0])
+
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			fmt.Print((*hugeMap)[y][x].id, " ")
+		}
+		fmt.Println()
+	}
+
+}
+
+func getMatchingTile(t tile, border int, hash *map[int]tile) (tile, int) {
+	borders := t.borders[border]
+	for k, v := range *hash {
+		if k == t.id {
+			continue
+		}
+		for i, b := range v.borders {
+			switch border {
+			case 2:
+				if borders[0] == b[0] {
+					switch i {
+					// no need to rotate
+					case 0:
+						return v, k
+					case 1, 2:
+						return flip(rotate(v, i), 'V'), k
+					case 3:
+						return rotate(v, 3), k
+					}
+				}
+				if borders[0] == b[1] {
+					switch i {
+					case 0:
+						return flip(v, 'V'), k
+					case 3:
+						return flip(rotate(v, 3), 'V'), k
+					default:
+						return rotate(v, i), k
+					}
+				}
+			case 3:
+				if borders[0] == b[0] {
+					switch i {
+					case 0:
+						return flip(rotate(v, 3), 'H'), k
+					case 1:
+						return v, k
+					case 2:
+						return rotate(v, 1), k
+					case 3:
+						return flip(rotate(v, 2), 'H'), k
+					}
+				}
+				if borders[0] == b[1] {
+					switch i {
+					case 0:
+						return rotate(v, 3), k
+					case 1:
+						return flip(v, 'H'), k
+					case 2:
+						return flip(rotate(v, 1), 'H'), k
+					case 3:
+						return rotate(v, 2), k
+					}
+				}
+			}
+		}
+	}
+	return tile{}, -1
 }
--- 2020/20/main_test.go
+++ 2020/20/main_test.go
@@ -1,17 +1,19 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPt1(t *testing.T) {
 	output := pt1(getInput())
-	if output != 220 {
+	if output != 23386616781851 {
 		t.Error("Invalid output:", output)
 	}
 }
 
 func TestPt2(t *testing.T) {
 	output := pt2(getInput())
-	if output != 439 {
+	if output != 23386616781851 {
 		t.Error("Invalid output:", output)
 	}
 }
@@ -21,4 +23,164 @@ func TestToInt(t *testing.T) {
 	if output != 9 {
 		t.Error("Invalid output:", output)
 	}
+}
+
+func TestRotate(t *testing.T) {
+	tmp := tile{
+		1,
+		[][]bool{
+			[]bool{true, false, false, false},
+			[]bool{true, true, false, false},
+			[]bool{true, true, true, false},
+			[]bool{true, true, false, true},
+		},
+		map[int][]int{},
+	}
+	tmp.borders[0] = []int{8, 1}
+	tmp.borders[1] = []int{12, 3}
+	tmp.borders[2] = []int{14, 7}
+	tmp.borders[3] = []int{13, 11}
+	//
+
+	rotated := rotate(tmp, 1)
+	if rotated.borders[0][0] != 3 || rotated.borders[0][1] != 12 {
+		t.Error("Wrong")
+	}
+	if rotated.borders[1][0] != 14 || rotated.borders[1][1] != 7 {
+		t.Error("Wrong")
+	}
+	if rotated.borders[2][0] != 11 || rotated.borders[2][1] != 13 {
+		t.Error("Wrong")
+	}
+	if rotated.borders[3][0] != 8 || rotated.borders[3][1] != 1 {
+		t.Error("Wrong")
+	}
+
+	for k, v := range [][]bool{
+		[]bool{false, false, false, true},
+		[]bool{false, false, true, false},
+		[]bool{false, true, true, true},
+		[]bool{true, true, true, true},
+	} {
+		for i, vv := range v {
+			if rotated.lines[k][i] != vv {
+				t.Error("Whops")
+			}
+		}
+	}
+
+}
+
+func TestFlip(t *testing.T) {
+	tmp := tile{
+		1,
+		[][]bool{
+			[]bool{true, false, false, false},
+			[]bool{true, true, false, false},
+			[]bool{true, true, true, false},
+			[]bool{true, true, false, true},
+		},
+		map[int][]int{},
+	}
+	tmp.borders[0] = []int{8, 1}
+	tmp.borders[1] = []int{12, 3}
+	tmp.borders[2] = []int{14, 7}
+	tmp.borders[3] = []int{13, 11}
+	//
+
+	flipped := flip(tmp, 'H')
+	for k, v := range [][]bool{
+		[]bool{false, false, false, true},
+		[]bool{false, false, true, true},
+		[]bool{false, true, true, true},
+		[]bool{true, false, true, true},
+	} {
+		for i, vv := range v {
+			if flipped.lines[k][i] != vv {
+				t.Error("Whops")
+			}
+		}
+	}
+
+	if tmp.borders[0][0] != 14 || tmp.borders[0][1] != 7 {
+		t.Error("Wrong")
+	}
+
+	if tmp.borders[2][0] != 8 || tmp.borders[2][1] != 1 {
+		t.Error("Wrong")
+	}
+
+	if tmp.borders[1][0] != 3 || tmp.borders[1][1] != 12 {
+		t.Error("Wrong")
+	}
+
+	if tmp.borders[3][0] != 11 || tmp.borders[3][1] != 13 {
+		t.Error("Wrong")
+	}
+}
+
+func TestFlipped2(t *testing.T) {
+	tmp := tile{
+		1,
+		[][]bool{
+			[]bool{true, false, false, false},
+			[]bool{true, true, false, false},
+			[]bool{true, true, true, false},
+			[]bool{true, true, false, true},
+		},
+		map[int][]int{},
+	}
+	tmp.borders[0] = []int{8, 1}
+	tmp.borders[1] = []int{12, 3}
+	tmp.borders[2] = []int{14, 7}
+	tmp.borders[3] = []int{13, 11}
+
+	flipped := flip(tmp, 'V')
+	for k, v := range [][]bool{
+		[]bool{true, true, false, true},
+		[]bool{true, true, true, false},
+		[]bool{true, true, false, false},
+		[]bool{true, false, false, false},
+	} {
+		for i, vv := range v {
+			if flipped.lines[k][i] != vv {
+				t.Error("Whops")
+			}
+		}
+	}
+
+	if tmp.borders[0][0] != 1 || tmp.borders[0][1] != 8 {
+		t.Error("Wrong")
+	}
+
+	if tmp.borders[2][0] != 7 || tmp.borders[2][1] != 14 {
+		t.Error("Wrong")
+	}
+
+	if tmp.borders[1][0] != 13 || tmp.borders[1][1] != 11 {
+		t.Error("Wrong")
+	}
+
+	if tmp.borders[3][0] != 12 || tmp.borders[3][1] != 3 {
+		t.Error("Wrong")
+	}
+}
+
+func TestOutput(t *testing.T) {
+	/*
+		After sorting, this is the resulting matrix.
+		Can be outputted in 4 orientations by chance.
+		1399 1637 3373 3169 3011 2473 3253 2203 3847 1193 1949 2731
+		3727 2089 1249 1747 2777 1063 2039 2131 1699 1009 3803 2927
+		1811 2797 2437 1429 1621 1307 3371 3719 1861 1069 3631 3593
+		1889 3709 3433 3701 3797 2099 1471 2621 1097 3083 3863 1103
+		2837 1753 3313 3527 2851 1361 2887 2677 1907 3079 1013 1117
+		1409 2503 1297 2647 1559 3697 1091 3323 3319 3359 1439 1181
+		2707 2659 1667 2801 2861 2477 2069 1787 2029 2179 1381 3613
+		1229 3203 2551 2273 2767 2243 2293 3533 1597 1367 1129 2879
+		2543 3943 3121 2617 3761 2843 1151 2113 2539 1583 1019 1451
+		3643 1579 2129 2971 3041 2423 1831 1327 3517 1171 2347 1913
+		1031 2953 2939 1231 1283 3989 1453 2609 1879 2557 1619 2531
+		3607 1289 3347 2143 1567 3413 3691 3911 2593 2657 3217 1697
+	*/
 }
